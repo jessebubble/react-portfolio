054beeed49873d206be2073931c9c216cc44f292
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-03-26 18:26:15 -0500

    header info added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Header(){
     return (
-        <section>
-            <h1 id='about'>Who am I?'</h1>
+        <section class='hero'>
+            <div class='block-header'><h1>Software Engineer in Training</h1></div>
+            <div class='block-info'>
+                <p>
+                Software Engineer with strong interpersonal and communication skills. <br></br>
+                Holds a Full Stack Web Development certificate from the University of Texas, San Antonio. <br></br>
+                Working in the highly regulated world of insurance taught me how to adapt in a changing professional environment. <br></br>
+                A strength that transitions into the world of web development when collaborating on projects and working with clients.
+                </p>
+            </div>
         </section>
     );
 }
