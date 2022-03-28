257ace73e2babc07ed3ded299c0c44e79099a539
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-03-28 01:17:34 -0500

    style added to about compnent

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/layout';
+
 
 function About(){
     return (
-        <section id='about-me' class='about-me'>
+        <Flex w='100%' maxWidth={{ base: '100vh', md: '130vh', lg: '130vh', xl: '130vh' }}>
+            <Box alignself='center' px='32' py='16'>
+                <Heading fontWeight='extrabold' color='cyan.500' size='4xl'>
+                    Full Stack Web Developer
+                </Heading>
+                <Text fontSize='2xl' color='gray.400'>Certificate from University of Texas, San Antonio</Text>
+            </Box>
+            <Box alignSelf='center' px='32' py='16'>
+                <Text fontWeight='bold' fontSize='2xl'>
+                Software Engineer with strong interpersonal and communication skills. <br></br>
+                Working in the highly regulated world of insurance taught me how to adapt in a changing professional environment. <br></br>
+                Looking forward to learning and growing with my new team of Software Engineers.
+                </Text>
+            </Box>
+        </Flex>
+
+
+    );
+}
+    
+export default About;
+
+/*         <section id='about-me' class='about-me'>
             <div class='block-header'><h2>About Me</h2></div>
             <div><img src={require(`../../assets/images/profilepic.jpeg`)} alt="" /></div>
             <div class='block-info about-me-info'>
                 <p>Software Engineer in Training</p>
             </div>
-        </section>
-    );
-}
-
-export default About;
+        </section> */
