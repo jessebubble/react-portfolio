18b5a09d19b038c14bb61ecee5af377284d68b3f
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-03-28 02:17:53 -0500

    coded out contact component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Contact from './components/Contact';
+/* import Contact from './components/Contact'; */
 import Nav from './components/Nav';
 
 
@@ -15,7 +15,7 @@ function App() {
       <Header></Header>
       <About></About>
       <Projects></Projects>
-      <Contact></Contact>
+{/*       <Contact></Contact> */}
       <Footer></Footer>
     </div>
   );
