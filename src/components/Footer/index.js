a6e3cbdb5ddad59bd9dcad2fc8838fcdb9d5b74a
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-03-28 01:31:51 -0500

    style added to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Icon from '@chakra-ui/icon';
+import { HStack, Text } from '@chakra-ui/layout';
+import { FaLinkedin, FaGithub, FaInstagram, FaMusic, FaCamera, FaTwitter, FaYoutube, FaVideo } from 'react-icons/fa';
 
-function Footer(){
+function Footer() {
     return (
-        <section id='contact-me' class='contact'>
+        <HStack spacing='24'>
+            <Icon as={FaLinkedin} boxSize='50' />
+            <Icon as={FaGithub} boxSize='50' />
+            <Icon as={FaInstagram} boxSize='50' />
+            <Icon as={FaMusic} boxSize='50' />
+            <Icon as={FaCamera} boxSize='50' />
+            <Icon as={FaTwitter} boxSize='50' />
+            <Icon as={FaYoutube} boxSize='50' />
+            <Icon as={FaVideo} boxSize='50' />
+            <Text>Jesse HernandezIII</Text>
+        </HStack>
+
+
+    );
+}
+    
+export default Footer;
+
+/*         <section id='contact-me' class='contact'>
             <div>
                 <h3>Online Presence</h3>
             </div>
@@ -10,8 +31,4 @@ function Footer(){
                 <a href='https://github.com/jessebubble'><img src={require(`../../assets/images/GitHub.png`)} alt=""/></a>
                 <a href='https://www.linkedin.com/in/jessebubble'><img src={require(`../../assets/images/linkedin.png`)} alt=""/></a>
             </div>
-        </section>
-    );
-}
-
-export default Footer;
+        </section> */
