5a8f8ac7536e28a7ad7a84948170bb292d4ef83e
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-04-19 15:46:44 -0500

    added cards to aboutMe

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useColorMode } from '@chakra-ui/color-mode';
-/* import { Button } from '@chakra-ui/button'; */
-/* import { Image } from '@chakra-ui/image'; */
 import { Stack, Flex, Box, Text } from '@chakra-ui/layout';
 
 function Header() {
@@ -13,7 +11,7 @@ function Header() {
         <Stack>
             <Flex alignSelf='center'>
                 <Box align='center'>
-                    <Text fontSize='5xl' fontWeight='semibold' bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'>🫧 Hello World! 🫧</Text>
+                    <Text fontSize='5xl' fontWeight='semibold' bgGradient='linear(to-l, cyan.400, blue.500, purple.600)' bgClip='text'>🫧 Hello World! 🫧</Text>
                     <Text fontSize='7xl' fontWeight='bold' bgGradient='linear(to-r, cyan.400, blue.500, purple.600)' bgClip='text'>MongoDB || ExpressJS || ReactJS || NodeJS</Text>
                     <Text color={isDark ? 'gray.200' : 'gray.500'}></Text>
                 </Box>
