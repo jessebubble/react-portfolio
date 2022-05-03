fce8e94fe818abea34db1fa4abf37efea0a0b7c6
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-05-03 00:03:01 -0500

    redid projects

---
+++ webdev-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import Projects from "./components/Projects";
+
+function App(){
+    return (
+        <div>
+            <Projects />
+
+        </div>
+    )
+}
+
+export default App;
---
+++ webdev-portfolio/src/components/Projects/index.jsx
@@ -0,0 +1,121 @@
+import "./projects.css";
+
+function Projects() {
+    return(
+        <section className="card-list">
+            <div className="card">
+                <div className="card-header">
+                    <p>ReactJS + Firebase + CSS</p>
+                    <h2>Memory Game 💡</h2>
+                </div>
+
+                <div className="card-logo">
+                    <a className="logo-avatar" href="https://pwa-memory-game.web.app/">
+                        <img src="https://images.unsplash.com/photo-1467991521834-fb8e202c7074?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHZpbnRhZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="project" />
+                    </a>
+                </div>
+
+                <div className="card-github">
+                    <a className="github-avatar" href="https://github.com/jessebubble/memory-game">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github"/>
+                    </a>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-header">
+                    <p>MongoDB + Express + React + Node</p>
+                    <h2>Parachute Play 🪂</h2>
+                </div>
+
+                <div className="card-logo">
+                    <a className="logo-avatar" href="https://jam-group-project.herokuapp.com/">
+                        <img src="https://images.unsplash.com/photo-1482773958144-74105d550cff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fHBhcmFjaHV0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="project" />
+                    </a>
+                </div>
+
+                <div className="card-github">
+                    <a className="github-avatar" href="https://github.com/jessebubble/parachute-play">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github"/>
+                    </a>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-header">
+                    <p>HTML5 + CSS3 + Javascript</p>
+                    <h2>Weather Dashboard 🌧️</h2>
+                </div>
+
+                <div className="card-logo">
+                    <a className="logo-avatar" href="https://jessebubble.github.io/weather-dashboard/">
+                        <img src="https://images.unsplash.com/photo-1489515217757-5fd1be406fef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHdlYXRoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="project" />
+                    </a>
+                </div>
+
+                <div className="card-github">
+                    <a className="github-avatar" href="https://github.com/jessebubble/weather-dashboard">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github"/>
+                    </a>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-header">
+                    <p>HTML5 + CSS3 + Javascript</p>
+                    <h2>Password Generator 🔑</h2>
+                </div>
+
+                <div className="card-logo">
+                    <a className="logo-avatar" href="https://jessebubble.github.io/password-generator/">
+                        <img src="https://github.com/jessebubble/password-generator/blob/main/assets/images/markus-spiske-uPXs5Vx5bIg-unsplash.jpg?raw=true" alt="project" />
+                    </a>
+                </div>
+
+                <div className="card-github">
+                    <a className="github-avatar" href="https://github.com/jessebubble/password-generator">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github"/>
+                    </a>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-header">
+                    <p>Coming Soon</p>
+                    <h2>Animal Farm 🦥</h2>
+                </div>
+
+                <div className="card-logo">
+                    <a className="logo-avatar" href="https://github.com/jessebubble/animal-farm">
+                        <img src="https://images.unsplash.com/photo-1588152850700-c82ecb8ba9b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YW5pbWFsJTIwZmFybXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="project" />
+                    </a>
+                </div>
+
+                <div className="card-github">
+                    <a className="github-avatar" href="https://github.com/jessebubble/animal-farm">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github"/>
+                    </a>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-header">
+                    <p>Coming Soon</p>
+                    <h2>Stripe Payments 💳</h2>
+                </div>
+
+                <div className="card-logo">
+                    <a className="logo-avatar" href="https://github.com/jessebubble/stripe-payments">
+                        <img src="https://images.unsplash.com/photo-1620714223084-8fcacc6dfd8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGF5bWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="project" />
+                    </a>
+                </div>
+
+                <div className="card-github">
+                    <a className="github-avatar" href="https://github.com/jessebubble/stripe-payments">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github"/>
+                    </a>
+                </div>
+            </div>
+
+            
+        </section>
+    );
+}
+
+export default Projects;
